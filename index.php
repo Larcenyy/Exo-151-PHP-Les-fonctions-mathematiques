7e00f952f3599bef3709d883f36337454ea73546
Rémy <remyc59440@hotmail.com>
2023-01-12 15:09:44 +0100

    da

--- index.php
+++ index.php
@@ -4,22 +4,25 @@
  * 1. Affichez dans un <p> la valeur absolue de -8
  */
 $value = -8;
+$exa = abs($value);
 // TODO Votre code ici.
-
+echo "<p>$exa</p>";
 
 /**
  * 2. Affichez dans un <p> la valeur de 5.64 arrondit au nombre supérieur.
  */
 $value = 5.64;
+$exa = ceil($value);
 // TODO Votre code ici.
-
+echo "<p>$exa</p>";
 
 /**
  * 3. Affichez dans un <p> la valeur de 5.34 arrondit au nombre inférieur.
  */
 $value = 5.34;
 // TODO Votre code ici.
-
+$exa = round($value);
+echo "<p>$exa</p>";
 
 /**
  * 4. Affichez dans un <p> la valeur maximum du tableau $max, et la valeur minimum du tableau $min.
@@ -27,7 +30,10 @@ $value = 5.34;
 $max = [5, 10, 12, 8, 7, 6, 54, 39, 76, 63, 84, 2, 21];
 $min = [5, 41, 65, 4, 3, 65, 2, 65, 74, 51, 23, 39, 42];
 // TODO Votre code ici.
-
+$exa = max($max);
+$exa2 = min($min);
+echo "<p>$exa</p>";
+echo "<p>$exa2</p>";
 
 /**
  * 5. Affichez dans un <p> la valeur de PI * 2 ( avec une fonction mathématique pour PI ).
@@ -35,13 +41,15 @@ $min = [5, 41, 65, 4, 3, 65, 2, 65, 74, 51, 23, 39, 42];
 
 // TODO Votre code ici.
 
-
+$exa = pi() * 2;
+echo "<p>$exa</p>";
 /**
  * 6. Affichez dans un <p> la valeur arrondie de 5.42 avec la fonction round().
  */
 $value = 5.42;
+$test = round($value);
 // TODO Votre code ici.
-
+echo "<p>$test</p>";
 
 
 
